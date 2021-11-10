04752bda4ce018920e5f9ee6b4f6fffe0963a7d5
y-ise <ise0615@gmail.com>
2021-11-10 19:45:01 +0900

    functions のTypescript対応

--- docker/firebase/src/functions/src/index.ts
+++ docker/firebase/src/functions/src/index.ts
@@ -1,17 +1,27 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+import {Request, Response, NextFunction} from "express";
+class HttpException extends Error {
+  statusCode?: number;
+  message: string;
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode || 500;
+    this.message = message;
+  }
+}
 const app: express.Express = express();
 
 var router = require('./routes');
 
 // Error Handling
-router.use((req, res, next) => {
+router.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
     error: 'Route Not Found'
   });
 });
 
-router.use((e, req, res, next) => {
+router.use((e: HttpException, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({
     error: e.name + ': ' + e.message
   });
