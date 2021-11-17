f3e0b40efe8da1c53809084403d6d4edd2eea436
iseyoshitaka <ise0615@gmail.com>
2021-11-17 17:04:49 +0900

    lintが成功するように修正

--- dc.sh
+++ dc.sh
@@ -15,17 +15,10 @@ Options:
   init                     Dockerコンテナ・イメージ・生成ファイルの状態を初期化します。
   start                    すべてのDaemonを起動します。
   stop                     すべてのDaemonを停止します。
-  apache restart           Apacheを再起動します。
-  mysql login              MySQLデータベースにログインします。
-  mysql export <PAHT>      MySQLデータベースのdumpファイルをエクスポートします。
-  mysql import <PAHT>      MySQLデータベースにdumpファイルをインポートします。
-  mysql restart            MySQLデータベースを再起動します。
-  php login                PHP-FPMのサーバーにログインします。
-  php cache                Laravelのキャッシュをクリアします。
-  php migrate              Laravelのマイグレードを実行します。
-  php seed                 Laravelのテストデータを登録します。
-  build                    フロントエンドをビルドします。
-  build prod               本番用にフロントエンドをビルドします。
+  firebase login           Firebase にログインします。
+  firebase start           Firebase のエミュレータを起動します。
+  firebase build           Cloud Functions をビルドします。
+  firebase deploy          Firebase にデプロイします。
   --version, -v     バージョンを表示します。
   --help, -h        ヘルプを表示します。
 EOF
